a167c60757ca8a0ea8a7188322f69ee60e5753c9
root <rodrigod.noriega@gmail.com>
2022-08-17 22:55:10 -0500

    debug with set command

---
+++ debug_set.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -n
+pwd
+date
+asaasdfasdf	#runtime error, code will continue execution
+user=$(whoami	#syntax error, program will break
+echo "This is about debugging a bash shell script"
+echo "The user is: $user"
