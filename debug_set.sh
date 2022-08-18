f1b53ea544700994410b79116f97082117063c75
root <rodrigod.noriega@gmail.com>
2022-08-17 23:07:12 -0500

    debug with set command

--- debug_set.sh
+++ debug_set.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 #set -x #use -x option for output step by step
-set -n
+#set -n
+#set -e
+#set -n
 pwd
 date
 asaasdfasdf	#runtime error, code will continue execution
