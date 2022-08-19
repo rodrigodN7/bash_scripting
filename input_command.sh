66943a619c9038504ac9026842a874d1d85be567
root <rodrigod.noriega@gmail.com>
2022-08-19 14:13:06 -0500

    input with arguments

---
+++ input_command.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#to see results execute in cli
+#eg ./input_command.sh arg1 arg2 argn
+
+echo ${0}
+echo ${1}
+echo ${2}
+echo ${3}
+echo "The number of comand line arguments are: $#"
+echo "The all command line arguments are: $@"
+echo "The all command line arguments are $*"
